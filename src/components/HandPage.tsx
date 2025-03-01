f740643b59ced80c81fbea30b3041d9e2d269e4d
itsyuimorii ♥ <109933604+itsyuimorii@users.noreply.github.com>
2025-03-01 13:39:35 -0600

    fixed the zod schema and data

--- src/components/HandPage.tsx
+++ src/components/HandPage.tsx
@@ -1,33 +1,36 @@
-import { useGestureWebsocket } from '../hook/useGestureWebSocket';
- 
+import { useGestureWebsocket } from "../hook/useGestureWebSocket";
 
 const HandPage = () => {
-  const { detectedHandGesture, isConnected, setIsConnected } = useGestureWebsocket();
-  console.log("🚀 ~ HandPage ~ detectedHandGesture:", detectedHandGesture)
-
+  const { parsedHands, isConnected, setIsConnected } = useGestureWebsocket();
 
   const handleConnection = () => {
     setIsConnected(!isConnected);
-  } 
+  };
 
   return (
-    <>
-      <div className="w-full h-screen flex items-center justify-center  p-4">
+    <div className="w-full h-screen flex items-center justify-center  p-4">
+      <h1 className="text-6xl">Hand.</h1>
 
-        <h1 className="text-6xl">Hand.</h1>
+      <div className="absolute bottom-8 left-8 bg-white p-4 rounded shadow">
+        {isConnected ? (
+          <p className="text-2xl">
+            Current Gesture:{" "}
+            {parsedHands.length
+              ? parsedHands[0].gestures[0].name
+              : "🪤 waiting..."}
+          </p>
+        ) : (
+          <p className="text-2xl">🐁 Please connecting....</p>
+        )}
 
-        <div className="absolute bottom-4 left-4 bg-white p-4 rounded shadow">
-          {/* <p>Current Gesture: {detectedHandGesture ? detectedHandGesture[0] : "Waiting..."}</p> */}
-          <button
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-            onClick={handleConnection}
-          >
-            {isConnected ? "Disconnect" : "Connect"}
-          </button>
-        </div>
+        <button
+          className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={handleConnection}
+        >
+          {isConnected ? "Disconnect" : "Connect"}
+        </button>
       </div>
-
-    </>
+    </div>
   );
 };
 
--- src/hook/useGestureWebSocket.ts
+++ src/hook/useGestureWebSocket.ts
@@ -1,41 +1,69 @@
 import { useState } from "react";
 import useWebSocket from "react-use-websocket";
-import zod from 'zod';
+import zod from "zod";
 
-const handsSchema = zod.array(zod.object({
-    gestures: zod.object({
+const handsSchema = zod.array(
+  zod.object({
+    gestures: zod.array(
+      zod.object({
         name: zod.string(),
-        confidence: zod.number(),
-    }),
-    hand: zod.object({
-        handedness: zod.string(),
-        keypoints: zod.array(zod.object({
-            x: zod.number(),
-            y: zod.number(),
-            z: zod.number(),
-        })),
-        keypoints3D  : zod.array(zod.object({
-            x: zod.number(),
-            y: zod.number(),
-            z: zod.number(),
-        })),
         score: zod.number(),
-    })
-}));
-
+      })
+    ),
+    hand: zod.object({
+      handedness: zod.string(),
+      keypoints: zod.array(
+        zod.object({
+          x: zod.number(),
+          y: zod.number(),
+          name: zod.string(),
+        })
+      ),
+      keypoints3D: zod.array(
+        zod.object({
+          x: zod.number(),
+          y: zod.number(),
+          z: zod.number(),
+          name: zod.string(),
+        })
+      ),
+      score: zod.number(),
+    }),
+  })
+);
 
 const SOCKET_URL = "ws://localhost:8081";
 
 export const useGestureWebsocket = () => {
-    const [isConnected, setIsConnected] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
 
-    const { lastMessage } = useWebSocket(SOCKET_URL, {
-        shouldReconnect: () => true,
-        onOpen: () => console.log("Connected"),
-        onClose: () => console.log("Disconnected"),
-    }, isConnected);
+  const { lastMessage } = useWebSocket(
+    SOCKET_URL,
+    {
+      shouldReconnect: () => true,
+      onOpen: () => console.log("Connected"),
+      onClose: () => console.log("Disconnected"),
+    },
+    isConnected
+  );
+  //const parsedHands = handsSchema.safeParse(lastMessage?.data).data || [];
+  type ParsedHandsType = zod.infer<typeof handsSchema>;
+  
+ 
 
-    const parsedHands = lastMessage ? handsSchema.safeParse(lastMessage).data : [];
+  let parsedHands: ParsedHandsType = [];
 
+  if(!lastMessage?.data){
     return { parsedHands, isConnected, setIsConnected };
-}
+  }
+  const parsedResult = handsSchema.safeParse(JSON.parse(lastMessage?.data));
+  console.log(  JSON.parse(lastMessage?.data))
+
+  if (parsedResult.success) {
+    parsedHands = parsedResult.data;
+  } else {
+    console.error(parsedResult.error);
+  }
+
+  return { parsedHands, isConnected, setIsConnected };
+};
