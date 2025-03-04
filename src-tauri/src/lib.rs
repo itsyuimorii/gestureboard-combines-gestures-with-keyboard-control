bb64ce0df600619711fa30d529b57fa3be4a0759
itsyuimorii ♥ <109933604+itsyuimorii@users.noreply.github.com>
2025-03-03 20:03:22 -0600

    update to enigo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,11 @@
-use mouce::{common::MouseButton, common::ScrollDirection, Mouse, MouseActions};
+// use ::{common::MouseButton, common::ScrollDirection, Mouse, MouseActions};
+use enigo::{
+    Button,
+    Direction,
+    Enigo, Mouse, Settings,
+    Coordinate,
+    Axis,
+};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -8,30 +15,36 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn move_relative(x: i32, y: i32) {
-    let mouse_manager = Mouse::new();
-    let _ = mouse_manager.move_relative(x, y);
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    enigo.move_mouse(x, y, Coordinate::Rel).unwrap();
 }
 
 #[tauri::command]
 fn mouse_left_click() {
-    let mouse_manager = Mouse::new();
-    let _ = mouse_manager.click_button(&MouseButton::Left);
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    enigo.button(Button::Left, Direction::Click).unwrap();
 }
 
 #[tauri::command]
 fn scroll_up() {
-    let mouse_manager = Mouse::new();
-    let _ = mouse_manager.scroll_wheel(&ScrollDirection::Left);
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+      enigo.scroll(2, Axis::Vertical).unwrap();
 }
 
 #[tauri::command]
 fn scroll_down() {
-    let mouse_manager = Mouse::new();
-    let _ = mouse_manager.scroll_wheel(&ScrollDirection::Right);
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+      enigo.scroll(-2, Axis::Vertical).unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[cfg(target_os = "windows")]
+    // This is needed on Windows if you want the application to respect the users scaling settings.
+    // Please look at the documentation of the function to see better ways to achive this and
+    // important gotchas
+    enigo::set_dpi_awareness().unwrap();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
